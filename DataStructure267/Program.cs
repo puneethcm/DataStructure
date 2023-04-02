52ba8c5321d175aded4a43b737a0b67c293c8956
puneethcm <puneethkumarcm8050@gmail.com>
2023-04-02 16:23:49 +0530

    Custome Linked List

--- DataStructure267/Program.cs
+++ DataStructure267/Program.cs
@@ -6,7 +6,7 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Data Structure");
-        DataStructure267.LinkedList<int> linkedList = new DataStructure267.LinkedList<int>();
+        DataStructure267.CustomeLinkedList<int> linkedList = new DataStructure267.CustomeLinkedList<int>();
         linkedList.Add(50);
         linkedList.Add(70);
         linkedList.AddFirst(40);
@@ -14,13 +14,13 @@ internal class Program
         linkedList.RemoveFist();
         linkedList.Search(70);
         linkedList.Display();
-        DataStructure267.LinkedList<string> linkedList1 = new DataStructure267.LinkedList<string>();
+        DataStructure267.CustomeLinkedList<string> linkedList1 = new DataStructure267.CustomeLinkedList<string>();
         Console.WriteLine("\n -------------------------------");
         linkedList1.Add("puneeth");
         linkedList1.Add("Abhi");
         linkedList1.AddFirst("Karthik");
         linkedList1.RemoveFist();
-        linkedList1.Search();
+        linkedList1.Search("Abhi");
         linkedList1.Display();
         Console.ReadLine();
     }
